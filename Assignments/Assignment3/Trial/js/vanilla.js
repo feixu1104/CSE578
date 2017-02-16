9854837ef3b476573aa9b6d220b2b2b0f7ea6d9c
Nikhil Lohia <nikoo28@gmail.com>
2017-02-16 13:58:49 -0700

    Added Visualisation of the day Assignment 3 Recordings for 14 and 16 Feb Lecture Slides

--- Assignments/Assignment3/Trial/js/vanilla.js
+++ Assignments/Assignment3/Trial/js/vanilla.js
@@ -1,1 +1,26 @@
 console.log("HELLO JS");
+var myNumber = 5;
+var myString = "Let us ";
+var combined = myNumber + myString;
+console.log(combined);
+var stringNumber = "15";
+var parsedNumber = +stringNumber;
+console.log(myNumber + parsedNumber);
+
+function square(x) {
+    return x * x;
+}
+
+console.log(square(25));
+
+d3.csv("./CoffeeData.csv", type, function (myArrayOfObjects) {
+    myArrayOfObjects.forEach(function (d) {
+        console.log(d.date + " " + d.profit);
+    });
+});
+
+function type(d) {
+    d.sales = parseFloat(d.sales);
+    d.profit = parseFloat(d.profit);
+    return d;
+}
--- Assignments/Assignment3/coffee-vis/js/coffee.js
+++ Assignments/Assignment3/coffee-vis/js/coffee.js
@@ -2,38 +2,104 @@ var chart;
 var height = 200;
 var width = 300;
 //DEFINE YOUR VARIABLES UP HERE
+var canvas;
+var margin = {top: 0, right: 50, bottom: 40, left: 20};
+var color = d3.scaleOrdinal(d3.schemeCategory10);
 
+/*
+Submission by: Nikhil Lohia (nlohia1@asu.edu)
+ID: 1211168085
+ */
 
 //Gets called when the page is loaded.
-function init(){
-  chart = d3.select('#vis').append('svg');
-  vis = chart.append('g');
-  //PUT YOUR INIT CODE BELOW
+function init() {
+    chart = d3.select('#vis').append('svg');
+    vis = chart.append('g');
+    //PUT YOUR INIT CODE BELOW
 
+    canvas = chart.attr("width", width)
+        .attr("height", height);
 }
 
 //Called when the update button is clicked
-function updateClicked(){
-  d3.csv('data/CoffeeData.csv',update);
+function updateClicked() {
+
+    d3.csv('data/CoffeeData.csv', update);
 }
 
 //Callback for when data is loaded
-function update(rawdata){
-  //PUT YOUR UPDATE CODE BELOW
+function update(rawdata) {
 
+    vis.selectAll("g").remove();
+    vis.selectAll("rect").remove();
 
+    //PUT YOUR UPDATE CODE BELOW
+    var xAxes;
+    var yAxes;
+
+    rawdata = d3.nest()
+        .key(function (d) {
+            return d[getXSelectedOption()];
+        })
+        .rollup(function (d) {
+            return d3.sum(d, function (g) {
+                return g[getYSelectedOption()];
+            });
+        }).entries(rawdata);
+
+    console.log(rawdata[0]);
+
+    xAxes = d3.scaleBand()
+        .range([margin.left, width - margin.right])
+        .padding(0.1)
+        .domain(rawdata.map(function (d) {
+            return d.key;
+        }));
+
+    yAxes = d3.scaleLinear()
+        .range([height - (margin.top + margin.bottom), 0])
+        .domain([0, d3.max(rawdata, function (d) {
+            return d.value;
+        })]);
+
+    vis.append("g")
+        .attr("transform", "translate(0," + (height - margin.top - margin.bottom) + ")")
+        .call(d3.axisBottom(xAxes));
+
+    vis.append("g")
+        .attr("transform", "translate(" + (width - margin.right) + ", 0)")
+        .call(d3.axisRight(yAxes).ticks(4));
+
+    vis.selectAll("rect")
+        .data(rawdata)
+        .enter().append("rect")
+        .attr("fill", function (d, i) {
+            return color(i);
+        })
+        .attr("x", function (d) {
+            return xAxes(d.key);
+        })
+        .attr("width", xAxes.bandwidth())
+        .attr("y", function (d) {
+            return yAxes(d.value);
+        })
+        .attr("height", function (d) {
+            return height - margin.top - margin.bottom - yAxes(d.value);
+        });
 }
 
 // Returns the selected option in the X-axis dropdown. Use d[getXSelectedOption()] to retrieve value instead of d.getXSelectedOption()
-function getXSelectedOption(){
-  var node = d3.select('#xdropdown').node();
-  var i = node.selectedIndex;
-  return node[i].value;
+function getXSelectedOption() {
+
+    var node = d3.select('#xdropdown').node();
+    var i = node.selectedIndex;
+    return node[i].value;
 }
 
 // Returns the selected option in the X-axis dropdown. 
-function getYSelectedOption(){
-  var node = d3.select('#ydropdown').node();
-  var i = node.selectedIndex;
-  return node[i].value;
+function getYSelectedOption() {
+
+    var node = d3.select('#ydropdown').node();
+    var i = node.selectedIndex;
+    return node[i].value;
 }
