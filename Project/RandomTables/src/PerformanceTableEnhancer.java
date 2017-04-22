279079ec019016a238eab5ac0adb5b28e06c07c2
Nikhil Lohia <nikoo28@gmail.com>
2017-04-22 02:35:05 -0700

    Modified performancetable

--- Project/RandomTables/src/PerformanceTableEnhancer.java
+++ Project/RandomTables/src/PerformanceTableEnhancer.java
@@ -9,6 +9,7 @@ public class PerformanceTableEnhancer {
 
   public static void main(String[] args) throws IOException {
 
+    String matchTable = "tables/matchTable.csv";
     String playerTable = "tables/playerTable.csv";
     String performanceTableEnhanced = "tables/performanceTableEnhanced.csv";
     String performanceTable = "tables/performanceTable.csv";
@@ -21,15 +22,47 @@ public class PerformanceTableEnhancer {
       playerNameTeamMap.put(splits[0], splits[1]);
     }
 
+    bufferedReader = new BufferedReader(new FileReader(matchTable));
+    Map<String, String> matchIdWinLose = new HashMap<>();
+    Map<String, String> matchIdTeam1 = new HashMap<>();
+    Map<String, String> matchIdTeam2 = new HashMap<>();
+    while ((currentLine = bufferedReader.readLine()) != null) {
+      String[] splits = currentLine.split(",");
+      String winner = splits[splits.length - 2];
+      String matchId = splits[0];
+      String team1 = splits[1];
+      String team2 = splits[2];
+      matchIdWinLose.put(matchId, winner);
+      matchIdTeam1.put(matchId, team1);
+      matchIdTeam2.put(matchId, team2);
+    }
+
     FileWriter fileWriter = new FileWriter(performanceTableEnhanced);
     BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
 
     bufferedReader = new BufferedReader(new FileReader(performanceTable));
     while ((currentLine = bufferedReader.readLine()) != null) {
       String[] splits = currentLine.split(",");
+      String matchId = splits[0];
       String playerName = splits[1];
       String playerTeam = playerNameTeamMap.get(playerName);
-      bufferedWriter.write(currentLine + "," + playerTeam + "\n");
+      String winningTeam = matchIdWinLose.get(matchId);
+      String winLose = null;
+      String team1 = matchIdTeam1.get(matchId);
+      String team2 = matchIdTeam2.get(matchId);
+      String playedAgainst;
+      if (playerTeam == null || winningTeam == null)
+        continue;
+      if (playerTeam.equals(winningTeam))
+        winLose = "win";
+      else
+        winLose = "lose";
+
+      if (playerTeam.equals(team1))
+        playedAgainst = team2;
+      else
+        playedAgainst = team1;
+      bufferedWriter.write(currentLine + "," + playerTeam + "," + winLose + "," + playedAgainst + "\n");
     }
 
     bufferedReader.close();
---
+++ Project/RandomTables/src/StreamTableFilter.java
@@ -0,0 +1,74 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * Created by nikoo28 on 4/21/17.
+ */
+public class StreamTableFilter {
+
+  public static void main(String[] args) throws IOException {
+
+    String streamTable = "tables/streamTables/West IndiesStreamTable.csv";
+    BufferedReader bufferedReader = new BufferedReader(new FileReader(streamTable));
+
+    Map<String, Integer> playerNameCountMap = new HashMap<>();
+
+    String currentLine = null;
+    while ((currentLine = bufferedReader.readLine()) != null) {
+
+      String[] splits = currentLine.split(",");
+      String playerName = splits[0];
+      if (playerNameCountMap.containsKey(playerName)) {
+        int count = playerNameCountMap.get(playerName);
+        count++;
+        playerNameCountMap.put(playerName, count);
+        continue;
+      }
+
+      playerNameCountMap.put(playerName, 1);
+    }
+    Map<String, Integer> sortedPlayerNameCountMap = sortByValue(playerNameCountMap);
+    Set<String> topTen = new HashSet<>();
+
+    int count = 0;
+    for (Map.Entry<String, Integer> stringIntegerEntry : sortedPlayerNameCountMap.entrySet()) {
+      topTen.add(stringIntegerEntry.getKey());
+      count++;
+      if (count == 10)
+        break;
+    }
+
+    String performanceTableEnhanced = "tables/filterStreamTables/WestIndiesStream.csv";
+    FileWriter fileWriter = new FileWriter(performanceTableEnhanced);
+    BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+    bufferedReader = new BufferedReader(new FileReader(streamTable));
+    currentLine = null;
+    while ((currentLine = bufferedReader.readLine()) != null) {
+      String[] splits = currentLine.split(",");
+      String playerName = splits[0];
+      if (topTen.contains(playerName))
+        bufferedWriter.write(currentLine + "\n");
+    }
+
+    bufferedWriter.close();
+    bufferedReader.close();
+
+
+  }
+
+  public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map) {
+    List<Map.Entry<K, V>> list = new LinkedList<Map.Entry<K, V>>(map.entrySet());
+    Collections.sort(list, new Comparator<Map.Entry<K, V>>() {
+      public int compare(Map.Entry<K, V> o1, Map.Entry<K, V> o2) {
+        return (-1 * (o1.getValue()).compareTo(o2.getValue()));
+      }
+    });
+
+    Map<K, V> result = new LinkedHashMap<K, V>();
+    for (Map.Entry<K, V> entry : list) {
+      result.put(entry.getKey(), entry.getValue());
+    }
+    return result;
+  }
+
+}
